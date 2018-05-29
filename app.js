f6454ec4c1d7b0cc9537a74add4359a91cdea0e5
maryCastilloG <maryecg@gmail.com>
2018-05-29 13:26:19 -0400

    Cree archivo

---
+++ app.js
@@ -0,0 +1,33 @@
+let age = parseInt(prompt('¿Qué edad tienes?'));
+
+if(age == 0 & age==""){
+    alert('Error')
+}
+
+if(age >=1 & age <=3){
+    alert('Eres toddler')
+}
+if(age >3 & age<=5){
+    alert('Eres preschooler')
+}
+    
+ if(age >5 & age<= 12){
+     alert('Eres gradeschooler')
+}
+       
+if(age  >12 &age <=18){
+     alert('Eres teenager')
+ }
+           
+if(age >18 & age<=21){
+    alert('Eres young adult') 
+}
+if(age > 21){
+    alert('adult')   
+}
+    
+
+
+
+                
+                               
